9cc3630712aceb5d2be183329e85295b5eaf77c4
Alexander <aleksievisa@gmail.com>
2023-02-15 02:59:44 +0300

    fix3 header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import './App.scss'
-import {Header} from "../components/Header/Header";
+import {Header} from "./components/Header/Header";
 //@ts-ignore
 const tg = window.Telegram.WebApp
 
