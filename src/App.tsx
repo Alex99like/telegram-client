f2008ce22d50e7a8d59b80f44efe51a92f894ac8
Alexander <aleksievisa@gmail.com>
2023-02-15 09:02:35 +0300

    fix toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import './App.scss'
 import {Header} from "./components/Header/Header";
-import {useTelegram} from "../hooks/useTelegram";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
   const { tg, onToggleButton } = useTelegram()
